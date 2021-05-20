d80356efc8458dac6675c8714d52d2056447c003
Shehan Atukorala <shehanatuk@gmail.com>
2021-05-19 21:40:57 -0400

    added angular powered modal to profile.component.html

--- AngularApp/src/app/profile/profile.component.ts
+++ AngularApp/src/app/profile/profile.component.ts
@@ -2,16 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService, UserDetails } from '../authentication.service';
 //can import both classes and interfaces in the same import statement
 import { AuthGuardService } from '../auth-guard.service';
-
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+import { from } from 'rxjs';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  closeResult = ''
   details: UserDetails;//saving user details interface object in variable
-  constructor(private auth: AuthenticationService, private authGuard: AuthGuardService) { }
+  constructor(private auth: AuthenticationService, private authGuard: AuthGuardService, private modalService: NgbModal) { }
 
+  open(content){
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    })
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC){
+      return 'by pressing ESC';
+    }else if (reason == ModalDismissReasons.BACKDROP_CLICK){
+      return 'by clicking on a backdrop';
+    }else{
+      return `with: ${reason}`
+    }
+  }
   ngOnInit() {//on start get the details of the user that is currently logged in
     this.authGuard.canActivate();
     this.auth.profile().subscribe(user => {
